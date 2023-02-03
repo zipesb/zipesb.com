4471cb67effdd37cd388b086f4427fb242a58d6c
Benjamin Zipes <zipesb@gmail.com>
2023-02-03 04:59:30 -0500

    bug fix: removed useless console log

--- scripts/delaytext.js
+++ scripts/delaytext.js
@@ -1,6 +1,5 @@
 function delayText(visibility, target, delay) {
     setTimeout(function(){
-        console.log(document.getElementById(target).style.visibility)
         document.getElementById(target).style.visibility = visibility;
     },delay)
 }
